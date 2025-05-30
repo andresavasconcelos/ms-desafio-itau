3148634437dc1a9bf241d3ea456ee728a760c64b
andresavasconcelos <andresa.vasconcelos-30@hotmail.com>
2025-05-30 18:58:29 -0300

    main - first commit

---
+++ src/main/java/com/itau/ms_desafio_itau/MsDesafioItauApplication.java
@@ -0,0 +1,13 @@
+package com.itau.ms_desafio_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDesafioItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDesafioItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/ms_desafio_itau/MsDesafioItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.ms_desafio_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDesafioItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
