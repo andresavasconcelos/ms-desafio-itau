526148a1632c9609e6987471897cb5fa14da3f48
andresavasconcelos <andresa.vasconcelos-30@hotmail.com>
2025-05-31 00:20:47 -0300

    main - controller: inclui logs para aumentar a observabilidade de erros e restreamente em caso de incidente em producao

--- src/main/java/com/itau/ms_desafio_itau/controller/AutomovelController.java
+++ src/main/java/com/itau/ms_desafio_itau/controller/AutomovelController.java
@@ -4,6 +4,8 @@ import com.itau.ms_desafio_itau.DTO.AutomovelRequestDTO;
 import com.itau.ms_desafio_itau.DTO.AutomovelResponseDTO;
 import com.itau.ms_desafio_itau.service.IAutomovelService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -16,6 +18,8 @@ import java.util.List;
 @RequestMapping("/api")
 public class AutomovelController {
 
+    private static final Logger log = LoggerFactory.getLogger(AutomovelController.class);
+
     private final IAutomovelService service;
 
     public AutomovelController(IAutomovelService service) {
@@ -23,22 +27,32 @@ public class AutomovelController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> create(@Valid @RequestBody AutomovelRequestDTO requestDTO){
+    public ResponseEntity<Void> create(@Valid @RequestBody AutomovelRequestDTO requestDTO) {
+        log.info("Requisição recebida para criação de automóvl");
+
         service.create(requestDTO);
+
+        log.info("Automóvel criado com sucesso");
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping
-    public ResponseEntity<List<AutomovelResponseDTO>> listAll(){
+    public ResponseEntity<List<AutomovelResponseDTO>> listAll() {
+        log.info("Requisição para listar todos os automóveis");
 
         List<AutomovelResponseDTO> responseList = service.listaAll();
+
+        log.info("Retornando {} automóveis", responseList.size());
         return ResponseEntity.ok(responseList);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AutomovelResponseDTO> findById(@PathVariable Long id){
+    public ResponseEntity<AutomovelResponseDTO> findById(@PathVariable Long id) {
+        log.info("Requisição para buscar automóvel com id: {}", id);
 
         AutomovelResponseDTO automovel = service.findById(id);
+
+        log.info("Automóvel encontrado com id: {}", id);
         return ResponseEntity.ok(automovel);
     }
 }
